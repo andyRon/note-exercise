aceef9d4b87e7b99cd1352e1c0b2345d5bb6c366
Andy Ron <randy.njfu@gmail.com>
2015-01-07 01:47:11 +0800

    oc

--- ios/OC/03.06 word-length-3/03.06 word-length-3/main.m
+++ ios/OC/03.06 word-length-3/03.06 word-length-3/main.m
@@ -9,9 +9,15 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+    FILE *wordFile = fopen("/tmp/words.txt", "r");
+    char word[100];
+    
+    while (fgets(word,100,wordFile)) {
+        word[strlen(word) - 1] = '\0';
+        NSLog(@"%s is %lu characters long", word, strlen(word));
     }
+    
+    fclose(wordFile);
+ 
     return 0;
 }
