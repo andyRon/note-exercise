9a69c478b43b818b793096a5b86989d17c2e0a6d
Andy Ron <randy.njfu@gmail.com>
2015-01-06 23:19:25 +0800

    Initial Commit

---
+++ ios/OC/03.04 word-length-1/03.04 word-length-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  03.04 word-length-1
+//
+//  Created by andyron on 15/1/6.
+//  Copyright (c) 2015年 andyron. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
