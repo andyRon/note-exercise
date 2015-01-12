ec658711bc9e7c5a8df26136c54449f63fccd27b
Andy Ron <randy.njfu@gmail.com>
2015-01-13 01:50:22 +0800

    swift

---
+++ ios/swift/swift-start.playground/section-1.swift
@@ -0,0 +1,33 @@
+// Playground - noun: a place where people can play
+
+println("hello,world")
+
+let consant = 23
+var variable = 334
+
+let implicitInteger = 10
+let explicitDouble: Double = 20
+
+let explicitFloat: Float = 4
+
+let label = "andy ron"
+let width = 54
+let widthLabel = label + String(width)
+
+//let ww  = label + width
+
+let widthLabel2 = " my name is \(label)."
+
+var shoppingList = ["catfish","water","tulips","blue paint"]
+shoppingList[1]
+
+var occupations = ["Malcolm":"Captain", "Kaylee":"Mechanic",]
+
+occupations["Jayne"] = "Public Relations"
+
+let emptyArray = String[]()
+let emptyDictionary = Dictionary<String,Float>()
+
+let emptyArray2 = []
+let empty Dictionary2 = [:]
+
