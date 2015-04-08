479deee331f4724e6b4839887ffe3e0b0b4c2258
Andy Ron <randy.njfu@gmail.com>
2015-04-08 23:49:31 +0800

    php app

--- PHP/php-app接口开发/list.php
+++ PHP/php-app接口开发/list.php
@@ -1,8 +1,8 @@
 <?php 
 //http://app.com/list.php?page=1&pagesize=12
-
+require_once("./response.php")
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $pageSize = isset($_GET['pagesize']) ? isset($_GET['pagesize']) : 1;
 if(!is_numeric($page) || !is_numeric($pageSize)){
-	
+	return Response::show(401,'数据不合法');		//记得加return，虽然show中又exit，但return让代码更可读
 }
