146572bf53282d1aeae5920dba01b0a44ff1968a
Andy Ron <randy.njfu@gmail.com>
2015-11-11 19:04:59 +0800

    c

--- pointers_on_c/10/10.11.2.c
+++ pointers_on_c/10/10.11.2.c
@@ -5,6 +5,30 @@ struct SALES_STATUS {
     char model[21];
     enum { CASH, LOAN, LEASE} type;
     union  {
-
+        struct {
+            float retail_price;
+            float selling_price;
+            float sales_tax;
+            float licensing_fee;
+        }cash;
+        struct {
+            float retail_price;
+            float selling_price;
+            float sales_tax;
+            float licensing_fee;
+            float down_payment;
+            float lan_duration;
+            float interest_rate;
+            float monthly_payment;
+            float bank;
+        }loan;
+        struct {
+            float retail_price;
+            float selling_price;
+            float down_payment;
+            float security_deposit;
+            float monthly_payment;
+            float lease_term;
+        }LEASE;
     }deal;
 };
