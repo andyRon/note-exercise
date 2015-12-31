818cf65327b6a7cecb39db1088e58b532bc7b349
Andy Ron <randy.njfu@gmail.com>
2015-12-31 18:13:32 +0800

    my-Yii2

--- PHP/Yii2/my-Yii2/vendor/yiisoft/yii2/caching/Cache.php
+++ PHP/Yii2/my-Yii2/vendor/yiisoft/yii2/caching/Cache.php
@@ -33,9 +33,136 @@ abstract class Cache extends Component implements \ArrayAccess
     {
         $key = $this->buildKey($key);
         $value = $this->getValue($key);
-        if (1) {
-            
+        if ($value === false || $this->serializer === false) {
+            return $value;
+        } elseif ($this->serializer === null) {
+            $value = unserialize($value);
+        } else {
+            $value = call_user_func($this->serializer[1], $value);
         }
+        if (is_array($value) && !($value[1] instanceof Dependency && $value[1]->getHasChanged($this))) {
+            return $value[0];
+        } else {
+            return false;
+        }
+    }
+    
+    public function exists($key)
+    {
+        $key = $this->buildKey($key);
+        $value = $this->getValue($key);
+        
+        return $value !== false;
+    }
+    
+    public function mget($keys)
+    {
+        $keyMap = [];
+        foreach ($keys as $key) {
+            $keyMap[key] = $this->buildKey($key);
+        }
+        $values = $this->getValues(array_values($keyMap));
+        $results = [];
+        foreach ($keyMap as $key => $newKey) {
+            $results[$key] = false;
+            if (isset($values[$newKey])) {
+                if ($this->serializer === false) {
+                    $results[$key] = $values[$newKey];
+                } else {
+                    $value = $this->serializer === null ? unserialize($values[$newKey]) : call_user_func($this->serializer[1], $values[$newKey]);
+                    
+                    if (is_array($value) && !($value[1] instanceof Dependency && $value[1]->getHasChanged($this))) {
+                        $results[$key] = $value[0];
+                    }
+                }
+            }
+        }
+        return $results;       
+    }
+    
+    public function set($key, $value, $duration = 0, $dependency = null) 
+    {
+        if ($dependency !== null && $this->serializer !== false) {
+            $dependency->evaluateDependency($this);
+        }
+        
+        if ($this->serializer === null) {
+            $value = serialize([$value, $dependency]);
+        } elseif ($this->serializer !== false) {
+            $value = call_user_func($this->serializer[0], [$value, $dependency]);
+        }
+        
+        $key = $this->buildKey($key);
+        $this->setValue($key, $value, $duration);
+    }
+    
+    public function mset($items, $duration = 0, $dependency = null)
+    {
+        if ($dependency !== null && $this->serializer !== false) {
+            $dependency->evaluateDependency($this);
+        }
+    
+        $data = [];
+        foreach ($items as $key => $value) {
+            if ($this->serializer === null) {
+                $value = serialize([$value, $dependency]);
+            } elseif ($this->serializer !== false) {
+                $value = call_user_func($this->serializer[0], [$value, $dependency]);
+            }
+    
+            $key = $this->buildKey($key);
+            $data[$key] = $value;
+        }
+    
+        return $this->setValues($data, $duration);
+    }
+    
+    public function madd($items, $duration = 0, $dependency = null)
+    {
+        if ($dependency !== null && $this->serializer !== false) {
+            $dependency->evaluateDependency($this);
+        }
+    
+        $data = [];
+        foreach ($items as $key => $value) {
+            if ($this->serializer === null) {
+                $value = serialize([$value, $dependency]);
+            } elseif ($this->serializer !== false) {
+                $value = call_user_func($this->serializer[0], [$value, $dependency]);
+            }
+    
+            $key = $this->buildKey($key);
+            $data[$key] = $value;
+        }
+    
+        return $this->addValues($data, $duration);
+    }
+    
+    public function add($key, $value, $duration = 0, $dependency = null)
+    {
+        if ($dependency !== null && $this->serializer !== false) {
+            $dependency->evaluateDependency($this);
+        }
+        if ($this->serializer === null) {
+            $value = serialize([$value, $dependency]);
+        } elseif ($this->serializer !== false) {
+            $value = call_user_func($this->serializer[0], [$value, $dependency]);
+        }
+        $key = $this->buildKey($key);
+    
+        return $this->addValue($key, $value, $duration);
+    }
+    
+    public function delete($key)
+    {
+        $key = $this->buildKey($key);
+    
+        return $this->deleteValue($key);
+    }
+    
+    public function flush()
+    {
+        $this->flushVlues();
     }
     
     abstract protected function getValue($key);
@@ -47,4 +174,60 @@ abstract class Cache extends Component implements \ArrayAccess
     abstract protected function deleteValue($key);
     
     abstract protected function flushVlues();
+    
+    protected function getValues($keys)
+    {
+        $results = [];
+        foreach ($keys as $key) {
+            $results[$key] = $this->getValue($key);
+        }    
+        return $results;
+    }
+    
+    protected function setValues($data, $duration)
+    {
+        $failedKeys = [];
+        foreach ($data as $key => $value) {
+            if ($this->setValue($key, $value, $duration) === false) {
+                $failedKeys[] = $key;
+            }
+        }
+        
+        return $failedKeys;
+    }
+    
+    protected function addValues($data, $duration)
+    {
+        $failedKeys = [];
+        foreach ($data as $key => $value) {
+            if ($this->addValue($key, $value, $duration) === false) {
+                $failedKeys = $key;
+            }
+        }
+        return $failedKeys;
+    }
+    
+    public function offsetExists($key) 
+    {
+        return $this->get($key) !== false;
+    }
+
+    public function offsetGet($key)
+    {
+        return $this->get($key);
+    }
+    
+    public function offsetSet($key, $value)
+    {
+        $this->set($key, $value);
+    }
+    
+    public function offsetUnset($key)
+    {
+        $this->delete($key);
+    }
+    
+    
+    
+    
 }
---
+++ PHP/Yii2/my-Yii2/vendor/yiisoft/yii2/caching/FileCache.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace yii\caching;
+
+use Yii;
+use yii\helpers\FileHelper;
+
+
+class FileCahce extends Cache
+{
+    public $keyPrefix = '';
+
+    public $cachePath = "@runtime/cache";
+
+    public $cacheFileSuffix = '.bin';
+
+    public $directoryLevel = 1;
+
+    public $gcProbability = 10;
+
+    /**
+    *   chmod() 
+    */
+    public $fileMode;
+
+    public $dirMode = 0775;
+
+    public function init()
+    {
+        parent::init();
+        $this->cachePath = Yii::aliases($this->cachePath);
+        if (!is_dir($this->cachePath)) {
+            FileHelper::createDirectory($this->cachePath, $this->dirMode, true);
+        }
+    }
+    // ?
+    public function exists($key)
+    {
+        $cacheFile = $this->getCacheFile($key);
+
+        return @filemtime($cacheFile) > time();
+    }
+
+    protected function getValue($key)
+    {
+        $cacheFile = $this->getCacheFile($$key);
+        if (@filemtime($cacheFile) > time()) {
+            return @file_get_contents($cacheFile);
+        } else {
+            return false;
+        }
+    }
+
+    protected function setValue($key, $value, $duration)
+    {
+        $cacheFile = $this->getCacheFile($key);
+        if ($this->directoryLevel > 0) {
+            @FileHelper::createDirectory(dirname($cacheFile), $this->dirMode, true);
+        }
+        if (@file_put_contents($cacheFile, $value, LOCK_EX) !== false) {
+            if ($this->fileMode !== null) {
+                @chmod($cacheFile, $this->fileMode);
+            }
+            if ($duration <= 0) {
+                $duration = 31536000;
+            }
+            // ?? 
+            return @touch($cacheFile, $duration + time());
+        } else {
+            return false;
+        }
+    }
+
+    protected function addValue($key, $value, $duration) 
+    {
+        $cacheFile = $this->getCacheFile($key);
+        if (@filemtime($cacheFile) > time()) {
+            return false;
+        }
+
+        $this->setValue($key, $value, $duration);
+    }
+
+    protected function deleteValue($key) 
+    {
+        $cacheFile = $this->getCacheFile($key);
+
+        @unlink($cacheFile);
+    }
+
+    protected function getCacheFile($key)
+    {
+        
+    }
+}
