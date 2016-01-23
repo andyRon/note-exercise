e1de9a1204fbb630f19b22a8dffcf3f0611d3ef7
Andy Ron <randy.njfu@gmail.com>
2016-01-23 08:29:37 +0800

    mix

--- ios/精通iOS开发/4 Control Fun/4 Control Fun/ViewController.m
+++ ios/精通iOS开发/4 Control Fun/4 Control Fun/ViewController.m
@@ -11,6 +11,10 @@
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *numberField;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property (weak, nonatomic) IBOutlet UISwitch *leftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *rightSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *dosomethingButton;
 
 @end
 
@@ -19,6 +23,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.sliderLabel.text = @"50";
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,4 +39,48 @@
     [self.nameField resignFirstResponder];
     [self.numberField resignFirstResponder];
 }
+- (IBAction)sliderChanged:(UISlider *)sender {
+    int progress = lroundf(sender.value);
+    self.sliderLabel.text = [NSString stringWithFormat:@"%d", progress];
+}
+- (IBAction)switchChanged:(UISwitch *)sender {
+    BOOL setting = sender.isOn;
+    [self.leftSwitch setOn:setting animated:YES];
+    [self.rightSwitch setOn:setting animated:YES];
+}
+- (IBAction)toggleControls:(UISegmentedControl *)sender {
+    if (sender.selectedSegmentIndex == 0) {
+        self.leftSwitch.hidden = NO;
+        self.rightSwitch.hidden = NO;
+        self.dosomethingButton.hidden = YES;
+    } else {
+        self.leftSwitch.hidden = YES;
+        self.rightSwitch.hidden = YES;
+        self.dosomethingButton.hidden = NO;
+    }
+}
+- (IBAction)buttonPressed:(id)sender {
+//    UIAlertController *actionSheet = [[UIAlertController alloc] initWithNibName]
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Are you sure?" delegate:self cancelButtonTitle:@"No Way!" destructiveButtonTitle:@"Yes, I'm Sure!" otherButtonTitles:nil];
+    [actionSheet showInView:self.view];
+}
+- (void)actionSheet:(UIActionSheet *)actionSheet
+    didDismissWithButtonIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex != [actionSheet cancelButtonIndex]) {
+        NSString *msg = nil;
+        
+        if ([self.nameField.text length] > 0) {
+            msg = [NSString stringWithFormat:@"You can breathe easy, %@, everything went OK.", self.nameField.text];
+        } else {
+            msg = @"You can breathe easy, everything went OK.";
+        }
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Something was done" message:msg delegate:self cancelButtonTitle:@"Phew!" otherButtonTitles:nil];
+        [alert show];
+    }
+    
+}
+
+
 @end
--- ios/精通iOS开发/精通iOS开发.md
+++ ios/精通iOS开发/精通iOS开发.md
@@ -1,5 +1,51 @@
 
 
+### 1
+- *沙盒*  应用用于存储文档、首选项等任何必要数据的地方
+
+### 2
+- Xcode 结构
+    1. toolbar
+        * 活动视图(activity view)   中间
+        * 右侧  用于切换
+            + standard editor
+            + assistant editor
+            + version editor
+    2. navigator
+        * project navigator(源代码文件、图片文件、数据模型、属性列表等)
+        * symbol navigator(编译器能识别的：OC类、枚举类型、结构体、全局变量)
+        * find navigator
+        * issues navigator
+        * test navigator
+        * debug navigator
+        * breakpoint navigator
+        * log navigator
+    3. jump bar(跳转栏)  
+        Recent Files, Unsaved Files, Counterparts(关联文件), Superclasses(父类), Subclasses, Siblings(兄弟类), Categories(类别), Include（引用文件)...
+    4. The Utility Area(实用工具面板，右侧)
+        * Attributes Inspector(属性检查器)
+        * Library
+            + file template
+            + code snippet
+            + object
+            + media
+    5. Interface Builder(界面构建器)
+    6. New Compiler and Debugger
+        - LLVM(Low Level Virtual Machine, 底层虚拟机)
+        - static analyzer(静态分析器)Xcode
+- Xcode快捷键和常用菜单
+    + `command + 0  ~  command + 8` 分别对应navigator
+    + `command + option + 0  ~  command + option + 6` 对应 Attributes Inspector
+    + `command + option + control + 0  ~  command + option + control + 4`  对应 Library
+    + `Command + Shift + o`  切换至某个文件或者找到特定的代码行
+    + `option + command + return`(View>Assistant Editor>Show Assistant Editor) 打开辅助编辑器
+    + `Editor>Canvas>Show Bounds Rectangles`   显示控件边框
+    + `Editor>Resovle Auto layout issues>Add Missing Constraints`  添加限制
+
+- 专有名词、概念等
+    - iOS Human Interface Guidelines(HIG)
+
+
 ### 3 实现基本交互
 
 1. MVC方法
@@ -11,3 +57,11 @@
     - 操作方法(action method) or 操作
     - 
 4. 应用程序委托
+
+### 4 
+Apply Retina 3.5-inch Form Factor
+
+http://developer.apple.com/library/ios/#samplecode/UICatalog/index.html
+
+http://learncocoa.org
+http://forum.learncocoa.org 
