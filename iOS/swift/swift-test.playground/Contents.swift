8e233bc080819d5c6b9406eeffd923a55d82ad0f
Andy Ron <randy.njfu@gmail.com>
2016-05-27 01:24:24 +0800

    swift

---
+++ iOS/swift/swift-test.playground/Contents.swift
@@ -0,0 +1,67 @@
+
+var table:String
+table = "andyron"
+
+let languageName = "swift"
+
+languageName.isEmpty
+languageName.hasPrefix("sw")
+languageName.hasSuffix("ft")
+languageName.lowercaseString
+
+"ab" + "bc"
+
+var a = 0xa
+
+var str = "戎"
+
+str.unicodeScalars
+
+var str2 = "Dog!🐶"
+var tmp = String()
+for s in str2.unicodeScalars {
+    tmp += " " + "\(s)"
+}
+print(tmp)
+for character in "Dog!🐶".characters {
+    print(character)
+}
+"🐶".unicodeScalars
+str2
+
+let catCharacters:[Character] = ["C", "a", "t"]
+let catString = String(catCharacters)
+
+var u = "\u{24}"
+"\u{1f496}"
+"\u{00e9}"
+
+
+"\u{496}"
+"\u{65}\u{301}"
+"\u{301}"
+
+"\u{1112}\u{1161}\u{11AB}".characters.count
+str2.characters.count
+str2
+str2.endIndex.predecessor()
+str2.startIndex
+str2[str2.endIndex.predecessor()]
+str2.startIndex.successor()
+tmp = ""
+for c in str2.characters {
+    tmp += "\(c)"
+}
+
+var arr:[String] = ["dog", "cat", "pig", "fish", "duck"]
+
+arr += ["egg", "dash"]
+arr[3...arr.count-1] = ["a"]
+arr
+arr.removeAtIndex(1)
+arr
+arr.removeLast()
+arr
+
+
+
