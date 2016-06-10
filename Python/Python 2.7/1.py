a9b16b125c2f288ef04fd5d71588753564f65d8c
Andy Ron <randy.njfu@gmail.com>
2016-06-10 23:42:59 +0800

    python

---
+++ Python/Python 2.7/1.py
@@ -0,0 +1,24 @@
+a = 10
+if a>=100:
+	print a
+else :
+	print -a
+
+print 1.23e9
+print 0x1f
+print "12\n"
+print '\\\t\\'
+print r'\\\t\\'
+
+print ''' adfa
+adf
+adf'''
+
+print True
+print 3 > 2
+
+print True or False
+
+print ord('A')
+
+print u'12'
