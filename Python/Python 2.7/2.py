da160bc58742627a58af7ae3bc7727ed9a6e4cb3
Andy Ron <randy.njfu@gmail.com>
2016-06-18 12:13:06 +0800

    python

--- Python/Python 2.7/2.py
+++ Python/Python 2.7/2.py
@@ -1,4 +1,6 @@
 def my_abs(x):
+	if not isinstance(x, (int, float)):
+		raise TypeError("bad operand type")
 	if x>=0:
 		return x
 	else: 
@@ -6,4 +8,26 @@ def my_abs(x):
 
 
 
-print my_abs(-1)
+# print my_abs("A")
+
+import math
+
+def move(x, y, step, angle):
+	dx = x + step * math.cos(angle)
+	dy = y + step * math.sin(angle)
+	return dx, dy
+
+x, y = move(100, 100, 60, math.pi/6)
+# print x, y
+
+
+
+def power(x, n=2):
+	s = 1
+	while n > 0:
+		n = n -1
+		s = s * x
+
+	return s
+
+print power(2, 5)
