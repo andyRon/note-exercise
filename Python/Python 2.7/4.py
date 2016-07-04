f08a2a9e1d158c83facd55e60b9f7220cde02945
Andy Ron <randy.njfu@gmail.com>
2016-07-04 23:51:50 +0800

    python

--- Python/Python 2.7/4.py
+++ Python/Python 2.7/4.py
@@ -6,4 +6,45 @@ l = range(8)
 
 # print map(fn, l)
 
-print str(12)
+# print str(12)
+
+def add(x, y):
+	return x + y;
+
+# print reduce(add, range(5))
+
+
+
+def fn2(x, y):
+	return x*10 + y
+
+
+# print reduce(fn2, [1, 3, 5, 7, 2])
+
+# print reduce(fn2, [23, 56])
+
+def char2num(s):
+	return {'0':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9}[s]
+
+def str2int(str):
+	return reduce(fn2, map(char2num, str))
+
+# print str2int("1254629347")
+
+def str2int(str):
+	return reduce(lambda x,y: x*10 +y, map(char2num, str))
+
+# print str2int("143434")
+def format(str):
+	return str[0].upper() + str[1:].lower()
+
+def lianxi_1(l):
+	return map(format, l)
+
+# print lianxi_1(['adam', 'LISA', 'barT'])
+
+def pro(l):
+	return reduce(lambda x,y: x*y, l)
+
+
+print pro([2,5,5,6])
--- Python/Python 2.7/Python 2.7.md
+++ Python/Python 2.7/Python 2.7.md
@@ -179,7 +179,9 @@
         + 编写高阶函数，就是让函数的参数能够接收别的函数。
     - map/reduce  （MapReduce: Simplified Data Processing on Large Clusters)[]
         + map()函数接收两个参数，一个是函数，一个是序列，map将传入的函数依次作用到序列的每个元素，并把结果作为新的list返回。
-        + reduce把一个函数作用在一个序列[x1, x2, x3...]上，这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算，其效果就是：  `reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)`
+        + reduce把一个函数作用在一个序列[x1, x2, x3...]上，这个函数必须接收两个参数，reduce把结果继续和序列的下一个元素做累积计算，其效果就是：    
+        `reduce(f, [x1, x2, x3, x4]) = f(f(f(x1, x2), x3), x4)`
+        
 
 
 
