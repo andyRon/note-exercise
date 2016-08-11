f06eea85b8b09f383ec7e4d4dd1575456fdde1b9
Andy Ron <randy.njfu@gmail.com>
2016-08-11 17:07:08 +0800

    max

---
+++ Python/Python 2.7/5.futrue.py
@@ -0,0 +1,9 @@
+# from __future__ import unicode_literals 
+
+print isinstance('xxx', unicode)
+
+print isinstance(u'xxx', unicode)
+
+print isinstance('xxx', str)
+
+print isinstance(b'xxx', str)
--- Python/Python 2.7/5.py
+++ Python/Python 2.7/5.py
@@ -10,10 +10,10 @@ import cStringIO as StringIO
 
 
 from PIL import Image
-im = Image.open("/Users/andyron/Downloads/bash.jpg")  #录取要是完整路径
-im.size
-im.mode
-im.format 
-# im.show()		# 以临时文件打开
+im = Image.open("/Users/andyron/Downloads/IMG_2900.PNG")  #录取要是完整路径
+print im.size
+# im.mode
+# im.format 
+im.show()		# 以临时文件打开
 
-print sys.path
+# print sys.path
