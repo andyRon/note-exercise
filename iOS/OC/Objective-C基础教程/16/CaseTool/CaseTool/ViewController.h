9b19eff0da221a8ba633ecf2237e41ff164c2b81
Andy Ron <randy.njfu@gmail.com>
2016-08-29 18:45:21 +0800

    iOS

---
+++ iOS/OC/Objective-C基础教程/16/CaseTool/CaseTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaseTool
+//
+//  Created by andyron on 16/8/29.
+//  Copyright © 2016年 andyron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS/OC/Objective-C基础教程/16/CaseTool/CaseTool/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CaseTool
+//
+//  Created by andyron on 16/8/29.
+//  Copyright © 2016年 andyron. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
