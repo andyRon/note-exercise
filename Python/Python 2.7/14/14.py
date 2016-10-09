cee666702452909436bf74710ab7350862d4de9b
Andy Ron <randy.njfu@gmail.com>
2016-10-09 10:27:11 +0800

    Python

---
+++ Python/Python 2.7/14/14.py
@@ -0,0 +1,20 @@
+from Tkinter import *
+
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.helloLabel = Label(self, text='Hello, world!')
+        self.helloLabel.pack()
+        self.quitButton = Button(self, text='Quit', command=self.quit)
+        self.quitButton.pack()
+
+
+app = Application()
+
+app.master.title('Hello World')
+
+app.mainloop()
