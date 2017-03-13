a4f3f92f3d1056965573c70a3eb86bc9458185b4
Andy Ron <randy.njfu@gmail.com>
2017-03-13 15:44:19 +0800

    max

---
+++ iOS/Beginning iOS 10 Programming with Swift/23/FoodPin/FoodPin/WebViewController.swift
@@ -0,0 +1,41 @@
+//
+//  WebViewController.swift
+//  FoodPin
+//
+//  Created by andyron on 2017/3/13.
+//  Copyright © 2017年 andyron. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let url = URL(string: "http://www.appcoda.com/contact") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
