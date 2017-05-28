122c86132e79b4359a8be0714fcef5a5880ec7fb
Andy Ron <randy.njfu@gmail.com>
2017-05-28 09:40:44 +0800

    Android start

---
+++ Android/MyFirstApp/app/src/main/java/com/example/andyron/myfirstapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.andyron.myfirstapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static com.example.andyron.myfirstapp.R.layout.activity_main;
+
+public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivtiy.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+}
--- network/computer_networking/computer_networking.md
+++ network/computer_networking/computer_networking.md
@@ -1,2 +1,12 @@
 《Computer Networking A Top-Down Approach》笔记
 ---------------
+
+## 1 Computer Networks and the Internet
+new Internet-connected devices
+end systems
+communication links
+packet switches
+routers   link-layer 
+- Home Access:
+    + DSL digital subscriber line
+    + cable
