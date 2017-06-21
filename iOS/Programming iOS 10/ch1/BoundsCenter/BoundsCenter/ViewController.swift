f2b55bb7c255890dfc9d05d8b6e00255ed02a13f
Andy Ron <randy.njfu@gmail.com>
2017-06-21 22:38:40 +0800

    mix

--- iOS/Programming iOS 10/ch1/BoundsCenter/BoundsCenter/ViewController.swift
+++ iOS/Programming iOS 10/ch1/BoundsCenter/BoundsCenter/ViewController.swift
@@ -12,24 +12,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let v1 = UIView(frame: CGRect(x: 113, y: 111, width: 132, height: 194))
+        v1.backgroundColor = UIColor(red: 1, green: 0.4, blue: 1, alpha: 1)
+        // insetBy表示产生一个同中心，但x，y中方向分别对称改变dx,dy大小的矩形，正数是减少，负数为增加
+        let v2 = UIView(frame: v1.bounds.insetBy(dx: 10, dy: 10))
+        v2.backgroundColor = UIColor(red: 0.5, green: 1, blue: 0, alpha: 1)
+        self.view.addSubview(v1)
+        v1.addSubview(v2)
+        
+        // 由于中心没变，所以导致v2和v1相同大小
+//        v2.bounds.size.height += 20
+//        v2.bounds.size.width += 20
+        
+        v1.bounds.origin.x += 10
+        v1.bounds.origin.y += 10
+        
+        print(v1.frame.origin, v1.center)
+        print(v2.frame.origin, v2.center)
+//        v2.bounds.origin.x -= 10
+//        v2.bounds.origin.y -= 10
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
