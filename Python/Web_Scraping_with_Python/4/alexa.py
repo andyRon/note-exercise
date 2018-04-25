6b8cacc7445106c67a675fc99f2f85a8b6f8082c
Andy Ron <randy.njfu@gmail.com>
2018-04-26 00:07:09 +0800

    python

--- Python/Web_Scraping_with_Python/4/alexa.py
+++ Python/Web_Scraping_with_Python/4/alexa.py
@@ -1,10 +1,15 @@
 import csv
 from zipfile import ZipFile
 from StringIO import StringIO
+# import three.downloader.Downloader
 from downloader import Downloader
 
 D = Downloader()
-zipped_data = D('')
+zipped_data = D('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip')
 urls = []
 with ZipFile(StringIO(zipped_data)) as zf:
     csv_filename = zf.namelist()[0]
+    for _, website in csv.reader(zf.open(csv_filename)):
+        urls.append('http://' + website)
+
+print urls
---
+++ Python/Web_Scraping_with_Python/4/downloader.py
@@ -0,0 +1,80 @@
+import urlparse
+import urllib2
+import random
+import time
+from datetime import datetime, timedelta
+import socket
+
+class Downloader:
+    def __init__(self, delay=5, user_agent='wswp', proxies=None, num_retries=1, opener=None, cache=None):
+        self.throttle = Throttle(delay)
+        self.user_agent = user_agent
+        self.proxies = proxies
+        self.num_retries = num_retries
+        self.opener = opener
+        self.cache = cache
+
+    def __call__(self, url):
+        result = None
+        if self.cache:
+            try:
+                result = self.cache[url]
+            except KeyError:
+                pass
+            else:
+                if self.num_retries > 0 and 500 <= result['code'] < 600:
+                    result = None
+        if result is None:
+            self.throttle.wait(url)
+            proxy = random.choice(self.proxies) if self.proxies else None
+            headers = {'User-agent': self.user_agent}
+            result = self.download(url, headers, proxy, self.num_retries)
+            if self.cache:
+                self.cache[url] = result
+        return result['html']
+
+    def download(self, url, headers, proxy, num_retries, data=None):
+        print 'Downloading:', url
+        request = urllib2.Request(url, data, headers or {})
+        opener = self.opener or urllib2.build_opener()
+        if proxy:
+            proxy_params = {urlparse.urlparse(url).scheme: proxy}
+            opener.add_handler(urllib2.ProxyHandler(proxy_params))
+        try:
+            response = opener.open(request)
+            html = response.read()
+            code = response.code
+        except Exception as e:
+            print 'Download error:', str(e)
+            html = ''
+            if hasattr(e, 'code'):
+                code = e.code
+                if num_retries > 0 and 500 <= code < 600:
+                    # retry 5XX HTTP errors
+                    return self._get(url, headers, proxy, num_retries-1, data)
+            else:
+                code = None
+        return {'html': html, 'code': code}
+
+
+
+
+class Throttle:
+    """Throttle downloading by sleeping between requests to same domain
+    """
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+        
+    def wait(self, url):
+        """Delay if have accessed this domain recently
+        """
+        domain = urlparse.urlsplit(url).netloc
+        last_accessed = self.domains.get(domain)
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                time.sleep(sleep_secs)
+        self.domains[domain] = datetime.now()
